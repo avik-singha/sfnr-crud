6c4d9beade6d4fc4809966eb6c8ba92974e1b0a9
Avik <aviksingha8@gmail.com>
2022-09-19 20:55:36 +0530

    swagger implementation

--- backend/server.js
+++ backend/server.js
@@ -9,8 +9,32 @@ const employeeRoute = require('./routes');
 const fastify = Fastify({
   logger: true
 })
-fastify.register(dbConnector)
+fastify.register(dbConnector) 
 fastify.register(employeeRoute)
+fastify.register(require('@fastify/swagger'),{  
+  routePrefix: '/documentation',
+  exposeRoute: true,
+  swagger: {
+      info: {
+          title: 'Fastify API',
+          description: 'Building a blazing fast REST API with Node.js, MySQL, Fastify and Swagger',
+          version: '1.0.0'
+      },
+      externalDocs: {
+          url: 'https://swagger.io',
+          description: 'Find more info here'
+      },
+      host: 'localhost',
+      schemes: ['http'],
+      consumes: ['application/json'],
+      produces: ['application/json']
+  }
+})
+
+fastify.ready(() => {
+  console.log('fastify server is ready')
+  fastify.swagger()
+  })
 
 fastify.listen({ port: 4000 }, function (err, address) {
   if (err) {
